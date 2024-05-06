2e37b7ffe5dc43805c13be808a9185d638b24255
Raf1517 <rr15rafa@hotmail.fr>
2024-05-06 18:11:33 +0200

    it security

--- app/_components/Veille.tsx
+++ app/_components/Veille.tsx
@@ -5,22 +5,21 @@ import { Code } from "./Code";
 export const Veille = () => {
   return (
     <Section className="flex flex-col items-start gap-4">
-      <Badge variant={"outline"}>Veille Technologique</Badge>
+      <Badge variant={"outline"}>Technology Watch</Badge>
       <h2 className=" text-3xl font-semibold tracking-tight first:mt-0">
-        Sécurité dans l{"'"}informatique
+        IT Security
       </h2>
       <div className="flex max-md:flex-col gap-4">
         <div className="flex flex-col gap-4 flex-1">
           <p className="text-sm text-muted-foreground">
-            La cybersécurité est très important dans le domaine informatique,
-            rester à jour sur les dernières vulnérabilités et les dernières
-            attaques est primordial pour éviter les attaques et les
-            vulnérabilités des applications et des sites nous développons. Je
-            maitiens donc une veille technlogiqueactive en
-            consultantrégulièrement les actualités sur ce sujet, en consultant
-            des sources d{"'"}informations telles que{" "}
-            <Code>Kaspersky Lab Threatpost</Code>,<Code>The Hacker News</Code>{" "}
-            et <Code>Schneier on Security</Code>.
+            Cybersecurity is very important in IT, staying up to date with the
+            latest vulnerabilities and attacks is essential to avoid attacks and
+            vulnerabilities in the vulnerabilities in the applications and sites
+            we develop. I therefore maintain an active technology watch by
+            regularly checking the latest news on the subject, and consulting
+            sources of information such as
+            <Code>Kaspersky Lab Threatpost</Code>,<Code>The Hacker News</Code>
+            and <Code>Schneier on Security</Code>.
           </p>
         </div>
       </div>
